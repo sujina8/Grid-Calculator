35fd8bd001a9be60ac15dd44c26be99c2594aa7f
sujina8 <165546019+sujina8@users.noreply.github.com>
2025-12-12 12:47:03 +0545

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package layout;
+
+public class Main {
+    public static void main(String[] args) {
+        //new FlowLayoutExample();
+        //new BorderLayoutExample();
+        new GridCalculator();
+    }
+}
